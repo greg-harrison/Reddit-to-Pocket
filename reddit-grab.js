8d411554ee0f882e4d33ef446b32862edf0f4ad1
Greg Harrison <gharrison@longviewinc.com>
2015-07-29 13:52:18 -0400

    Second commit. Major updates. Allows for arguments. Ex. node reddit-grab all 15, will return 15 results in a single listing. node reddit-grab each 2, will return 2 results from each of the default subreddits, node reddit-grab each 2 webdev+keto+cars, will return 2 from webdev, 2 from keto and 2 from cars

--- reddit-grab.js
+++ reddit-grab.js
@@ -4,23 +4,45 @@ var express = require('express'),
     http = require('http'),
     process = require('process'),
     moment = require('moment'),
-    prettyjson = require('prettyjson');
+    prettyjson = require('prettyjson'),
+    q = require('q');
 
-var subreddits = ['programming', 'webdev', 'opensource', 'frontend'];
-var subList = process.argv[2] || subreddits.toString().replace(/,/g, '+');
-var limit = 5;
+var subreddits = ['webdev', 'opensource', 'frontend', 'programming'];
+var arg2 = process.argv[2] || 'all';
+var subListDefault = setSubListDefault(arg2);
+var subList = process.argv[4] || subListDefault;
+var limit = process.argv[3] || 2;
 var timePeriod = "day";
+var timeOccurred = "";
 var pocketArray = [];
 
+function setSubListDefault (arg2) {
+    if (arg2 == 'each') {
+        return subreddits;
+    }
+    else {
+        return subreddits.toString().replace(/,/g, '+');
+    }
+}
 
-var options = {
-    url: "http://www.reddit.com/r/" + subList + "/top.json?t="+ timePeriod +"&limit=" + limit + ""
+var buildRequestUrl = function (arg2, subList, timePeriod, limit) {
+    if (arg2 == 'each') {
+        subList.forEach(function (element) {
+            var url = "http://www.reddit.com/r/" + element + "/top.json?t="+ timePeriod +"&limit=" + limit + "";
+            request(url, returnAll);
+        });
+    } else {
+        var url = "http://www.reddit.com/r/" + subList + "/top.json?t="+ timePeriod +"&limit=" + limit + "";
+        request(url, returnAll);
+    }
 };
 
-function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-        console.log('=======================================================');
+//var someOptions = {
+//    url: "http://www.reddit.com/r/" + subreddits[i] + "/top.json?t="+ timePeriod +"&limit=" + limit + ""
+//};
 
+function returnAll(error, response, body) {
+    if (!error && response.statusCode == 200) {
         var resp = JSON.parse(body);
         var respLength = resp.data.children.length;
 
@@ -31,6 +53,7 @@ function callback(error, response, body) {
             var titleData = "";
             var linkData = "";
             var tagData = "";
+            var timeData = "";
             var dateMonth = moment().format('MMMM');
             var dateYear = moment().format('YYYY');
 
@@ -51,6 +74,7 @@ function callback(error, response, body) {
                 titleData = resp.data.children[i].data.title;
                 linkData = resp.data.children[i].data.url;
                 tagData = "r/"+subredditData + ", " + dateYear + " " + dateMonth;
+                timeData = moment().format('X');
 
                 console.log('\nNumber: ', i+1);
                 console.log('Subreddit: ', subredditData);
@@ -58,20 +82,17 @@ function callback(error, response, body) {
                 console.log('Link: ', linkData);
                 console.log('Tags: ', tagData);
 
-                pocketObject.number = i+1;
-                pocketObject.subreddit = subredditData;
-                pocketObject.title = titleData;
-                pocketObject.link = linkData;
+                pocketObject.item_id = timeOccurred+"_"+i+1;
                 pocketObject.tags = tagData;
+                pocketObject.time = timeData;
+                pocketObject.title = titleData;
+                pocketObject.url = linkData;
 
                 pocketArray.push(pocketObject);
-
-                //Todo: package this block of info and ship it over to Pocket
             }
         }
 
         console.log("\n" + pocketArray.length + " items packaged in array");
-        console.log("\n" + options.url);
         console.log('\nLast execution: ' + moment().format("MM/D, h:mm:ss a"));
         console.log('=======================================================\n');
 
@@ -80,5 +101,14 @@ function callback(error, response, body) {
     }
 }
 
-request(options, callback);
+function sendToPocket(error, response, body){
 
+}
+
+buildRequestUrl(arg2, subList, timePeriod, limit);
+
+//var action = "add";
+//var
+//var pocketUrl = "https://getpocket.com/v3/send?actions=%5B%7B%22action%22%3A%22archive%22%2C%22time%22%3A1348853312%2C%22item_id%22%3A229279689%7D%5D&access_token=[ACCESS_TOKEN]&consumer_key=[CONSUMER_KEY]";
+//
+//request(, sendToPocket);
